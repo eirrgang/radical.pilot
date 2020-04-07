8f3d6795c47100c4743e2e56efb9e3aa83e91442
Mikhail Titov <mikhail.a.titov@gmail.com>
2020-04-06 23:00:59 -0400

    changed the offset-creation procedure in ibrun (test case for Longhorn)

--- src/radical/pilot/agent/launch_method/ibrun.py
+++ src/radical/pilot/agent/launch_method/ibrun.py
@@ -3,6 +3,7 @@
 __copyright__ = "Copyright 2016, http://radical.rutgers.edu"
 __license__   = "MIT"
 
+import os
 
 import radical.utils as ru
 
@@ -46,20 +47,23 @@ class IBRun(LaunchMethod):
         task_args    = cud.get('arguments') or []
         task_argstr  = self._create_arg_string(task_args)
 
-        cpn     = self._cfg.cores_per_node
-        index   = 0
+        # consider external environment variable that is set manually
+        # and reflects how many actual tasks per node were submitted
+        cpn = os.environ.get('TACC_TASKS_PER_NODE') or self._cfg.cores_per_node
+        index = 0
         offsets = list()
 
-        import pprint
+        # FIXME: remove assumption that every task would have the same number
+        #  of processes and threads (along with TACC_TASKS_PER_NODE it is
+        #  assumed that num_processes=1)
         for node in self._node_list:
             for slot_node in slots['nodes']:
                 if slot_node['uid'] == node[0]:
                     for core_map in slot_node['core_map']:
-                        for core_idx in core_map:
-                            offsets.append(index + core_idx)
-    
+                        if core_map:
+                            offsets.append(
+                                (core_map[0] // len(core_map)) + index)
             index += cpn
-
         ibrun_offset = min(offsets)
 
         if task_argstr:
